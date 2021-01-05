394cd056a2f29cc34a52e5f965e1fafe82789d97
@codejack <Kondasmajid@gmail.com>
2021-01-04 19:19:47 -1200

    import public file

--- app.js
+++ app.js
@@ -17,20 +17,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'public', 'bootstrap')));
-// app.use(express.static(path.join(__dirname, 'public', 'stylesheets')));
-
-// app.use('/static', express.static('public'))
-
 // var assetsPath = path.join(__dirname, '../client/assets');
 // app.use(express.static(assetsPath));
 
 app.use(express.static("public"));
-// app.use(express.static("public", "images"));
-
-// app.use('/public', express.static(path.resolve(__dirname, 'public')));
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
