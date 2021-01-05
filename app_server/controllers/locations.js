c5b8b5b31d46bae21f8036a1fe9b86adea8a73db
@codejack <Kondasmajid@gmail.com>
2021-01-04 18:57:09 -1200

    change layout funcs

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,24 +1,25 @@
 // get home page 
 
-module.exports.homelist = function(reg, res){
-    res.render('locations-list', {title: 'Home'})
+module.exports.homelist = function(req, res) {
+    res.render('locations-list', { title: 'Home' })
 }
-  
+
 // get location info page 
 
-module.exports.locationInfo = function(reg, res){
-    res.render('location-info', {title: 'Locaton Info'})
+module.exports.locationInfo = function(req, res) {
+    res.render('location-info', { title: 'Locaton Info' })
 }
 
 
 // get Review page 
 
-module.exports.addReview = function(reg, res){
-    res.render('location-review-form', {title: ' Add Review '});
+module.exports.addReview = function(req, res) {
+    res.render('location-review-form', { title: ' Add Review ' });
 }
 
 
-
+// get about page 
+// module.exports.
 
 
 // /* GET 'home' page */
@@ -26,25 +27,25 @@ module.exports.addReview = function(reg, res){
 // module.exports.homelist = function(req, res){
 
 //     res.render('locations-list', { title: 'Home' });
-  
+
 //   };
-  
-  
-  
+
+
+
 //   /* GET 'Location info' page */
-  
+
 //   module.exports.locationInfo = function(req, res){
-  
+
 //     res.render('location-info', { title: 'Location info' });
-  
+
 //   };
-  
-  
-  
+
+
+
 //   /* GET 'Add review' page */
-  
+
 //   module.exports.addReview = function(req, res){
-  
+
 //     res.render('location-review-form', { title: 'Add review' });
-  
+
 //   };
