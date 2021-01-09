b49637964bd117493b0f3f77b4e55828a114c539
@codejack <Kondasmajid@gmail.com>
2021-01-09 09:06:13 -1200

    assigned to specific paths

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,23 +1,36 @@
 // get home page
 
-module.exports.homelist = function(req, res) {
-    res.render("locations-list", { title: "Home" });
+// module.exports.homelist = function(req, res) {
+//     res.render("locations-list", { title: "Home" });
+// };
+const homelist = (req, res) => {
+    res.render("locations-list", {
+        title: "Loc8r = Find place to work with wifi",
+        pageHeader: {
+            title: "Loc8r",
+            strapline: "Fin places to work with wifi near you!",
+        },
+    });
 };
 
 // get location info page
 
-module.exports.locationInfo = function(req, res) {
-    res.render("location-info", { title: "Locaton Info" });
-};
-
-// const locationInfo = (req, res) => {
-//     res.render("location-info", { title: "Location info" });
+// module.exports.locationInfo = function(req, res) {
+//     res.render("location-info", { title: "Locaton Info" });
 // };
+
+const locationInfo = (req, res) => {
+    res.render("location-info", { title: "Location info" });
+};
 // module.exports.locationInfo;
 // get Review page
 
-module.exports.addReview = function(req, res) {
-    res.render("location-review-form", { title: " Add Review " });
+// module.exports.addReview = function(req, res) {
+//     res.render("location-review-form", { title: " Add Review " });
+// };
+
+const addReview = (req, res) => {
+    res.render("location-review-form", { title: "Add review" });
 };
 
 // get about page
